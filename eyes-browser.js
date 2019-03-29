a8146c6bf0700f02e2b1b51e607de359c7731e2b
Oleh Astappiev <astappev@gmail.com>
2019-03-29 15:03:27 +0100

    Add `testWindow` command which uses matchWindowSingle

--- eyes-browser.js
+++ eyes-browser.js
@@ -259,6 +259,29 @@ var EyesClient = /** @class */ (function () {
     return this.controlFlow;
   };
 
+  /**
+   * @param {string} [name]
+   * @return {TestResults}
+   */
+  Eyes.prototype.testWindow = function (name) {
+    var that = this;
+    console.debug('WctEyes: testWindow() - begin');
+    this.controlFlow = this.controlFlow.then(function () {
+      return new Promise(function (resolve) {
+        console.debug('WctEyes: testWindow() - begin promise');
+        that.eyesTop.getEmitter().once('eyes:testWindowDone', function (testResults) {
+          console.debug('WctEyes: testWindow() - testWindowDone', testResults);
+          if (testResults.passed) return resolve();
+          return reject(new Error(testResults.message));
+        });
+        console.debug('WctEyes: testWindow() - testWindow');
+        that.eyesTop.startCommand('eyes:testWindow', { sessionId: that.sessionId, name: name });
+      });
+    });
+
+    return this.controlFlow;
+  };
+
   Eyes.prototype.close = function (throwEx) {
     var that = this;
     console.debug('WctEyes: close() - begin');
